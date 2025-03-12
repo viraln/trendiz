71b301ae8532cca7d87943e66faeee18740cc31b
Saarule <saaryazdi@gmail.com>
2025-03-12 18:40:07 +0200

    Add _app.js and global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,5 @@
-import { useEffect } from 'react';
-import Script from 'next/script';
 import '../styles/globals.css'
-import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Trendiz - AI-Curated Tech Insights</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google-adsense-account" content="ca-pub-7120248945278154" />
-      </Head>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7120248945278154"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} />
-    </>
-  )
-}
-
-export default MyApp 
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+} 
